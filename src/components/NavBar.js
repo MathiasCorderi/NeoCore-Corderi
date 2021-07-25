2ce5fa905b1413d1558cb6921e6b1b58ceb92626
Mathias Corderi <matutecorderi@hotmail.com>
2021-07-25 15:14:18 -0300

    mi primer navbar que emoción

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+             <h1>Neocore</h1>
+        <ul>
+            <a href="#"><li>Productos</li></a>
+            <a href="#"><li>Sucursales</li></a>
+            <a href="#"><li>Contactos</li></a>
+
+        </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
