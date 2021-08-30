f56628d6278184f93daa4d747615938a47d8c5d2
Mathias Corderi <matutecorderi@hotmail.com>
2021-08-30 14:26:42 -0300

    Último desafío

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import { CartContext } from './CartContextProvider';
 import CartItem from './CartItem';
 import { Link } from 'react-router-dom';
-
+import { firestore } from '../firebase';
 
 const Cart = () => {
 
@@ -10,6 +10,19 @@ const Cart = () => {
 
    const limpiarCart2 = () => limpiarCart(cart, setCart)
     const limpiarItem2 = (e) => limpiarItem(e.target, cart, setCart)
+
+    
+       
+        const db = firestore;
+        const collection = db.collection('compras')
+
+        const usuario = {nombre: 'Enrique Antonio Neira', email: 'kikefoster4000@hotmail.com', compra: cart, total: total}
+
+        const query = () => {
+            collection.add(usuario).then((docRef) => {
+                console.log("Este es tu ID de compra: ", docRef.id)})
+            }
+                
   
   
     if (cart.length > 0) {
@@ -19,6 +32,7 @@ const Cart = () => {
             <CartItem nombre={p.nombre} inCart={p.inCart} total={p.precio * p.inCart} id={p.id} limpiar={limpiarItem2}/>)}
             <h3>Total: $ {total}.00</h3>
             <button onClick={limpiarCart2}>Limpiar el carrito</button> 
+            <button onClick={query}>Comprar!</button>
         </div>
     );
 
